ae99631fac96f1bc2155b837d3f018f4f426c0e0
X-sensei <77637156+X-sensei@users.noreply.github.com>
2023-07-10 10:45:34 +0530

    Update core.js

--- Plugins/core.js
+++ Plugins/core.js
@@ -5,50 +5,30 @@ let mergedCommands = [
   "help",
   "h",
   "menu",
-  "sc",
   "support",
   "supportgc",
-  "script",
+	"report",
 ];
 
 module.exports = {
   name: "others",
   alias: [...mergedCommands],
-  uniquecommands: ["sc", "support"],
+  uniquecommands: ["help", "support", "report"],
   description: "All miscleaneous commands",
   start: async (Atlas, m, { pushName, prefix, inputCMD, doReact }) => {
     let pic = fs.readFileSync("./Assets/Atlas.jpg");
     switch (inputCMD) {
-      case "script":
-      case "sc":
-        await doReact("🧣");
-        let repoInfo = await axios.get(
-          "https://api.github.com/repos/FantoX001/Atlas-MD"
-        );
-        let repo = repoInfo.data;
-        let txt = `            🧣 *${botName}'s Script* 🧣\n\n*🎀 Total Forks:* ${
-          repo.forks_count
-        }\n*⭐ Total Stars:* ${repo.stargazers_count}\n*📜 License:* ${
-          repo.license.name
-        }\n*📁 Repo Size:* ${(repo.size / 1024).toFixed(
-          2
-        )} MB\n*📅 Last Updated:* ${repo.updated_at}\n\n*🔗 Repo Link:* ${
-          repo.html_url
-        }\n\n❝ Dont forget to give a Star ⭐ to the repo. It's made with restless hardwork by *Team ATLAS*. ❞\n\n*©️ Team ATLAS- 2023*`;
-        Atlas.sendMessage(m.from, { image: pic, caption: txt }, { quoted: m });
-        break;
-
-      case "support":
+            case "support":
       case "supportgc":
-        await doReact("🔰");
-        let txt2 = `              🧣 *Support Group* 🧣\n\n*${botName}* is an open source project, and we are always happy to help you.\n\n*Link:* ${suppL}\n\n*Note:* Please don't spam in the group, and don't message *Admins directly* without permission. Ask for help inside *Group*.\n\n*Thanks for using Atlas.*`;
+        await doReact("🎀");
+        let txt2 = `              🧣 *Support Group* 🧣\n\n*${botName}* is a Public Bot, and we are always happy to help you.\n\n*Link:* ${suppL}\n\n*Note:* Please don't spam in the group, and don't message *Admins directly* without permission. Ask for help inside *Group*.\n\n*Thanks for using Elaina✨.*`;
         Atlas.sendMessage(m.from, { image: pic, caption: txt2 }, { quoted: m });
         break;
-
+		   
       case "help":
       case "h":
       case "menu":
-        await doReact("☃️");
+        await doReact("🎐");
         await Atlas.sendPresenceUpdate("composing", m.from);
         function readUniqueCommands(dirPath) {
           const allCommands = [];
@@ -83,12 +63,12 @@ module.exports = {
               file.replace(".js", "").charAt(0).toUpperCase() +
               file.replace(".js", "").slice(1);
 
-            formatted += `╟   🏮 *${capitalizedFile}* 🏮   ╢\n\n`;
+            formatted += `🔖 *${capitalizedFile}:*\n`;
             //formatted += `\`\`\`${commands.join("\n")}\`\`\`\n\n\n`;
             // Adding a - before each command
             formatted += `\`\`\`${commands
-              .map((cmd) => `⥼   ${prefix + cmd}`)
-              .join("\n")}\`\`\`\n\n\n`;
+              .map((cmd) => `${cmd}`)
+              .join(",")}\`\`\`\n\n`;
           }
 
           return formatted.trim();
@@ -98,7 +78,15 @@ module.exports = {
 
         const allCommands = readUniqueCommands(pluginsDir);
         const formattedCommands = formatCommands(allCommands);
-        var helpText = `\nKonnichiwa *${pushName}* Senpai,\n\nI am *${botName}*, a WhatsApp bot built to take your boring WhatsApp experience into next level.\n\n*🔖 My Prefix is:*  ${prefix}\n\n${formattedCommands}\n\n\n*©️ Team ATLAS- 2023*`;
+        var helpText = `\n「(💙^💙」
+│⋊ 𝕌𝕤𝕖𝕣: *${pushName}*
+│⋊ ℕ𝕒𝕞𝕖: *${botName}*
+│⋊ ℙ𝕣𝕖𝕗𝕚𝕩: *${prefix}*
+│⋊ 𝕆𝕨𝕟𝕖𝕣: *Ronen🎐*
+│⋊ 𝕆𝕗𝕗𝕚𝕔𝕚𝕒𝕝 𝔾𝕣𝕠𝕦𝕡: http://surl.li/eumln
+╰────────────┈エリーナ  
+
+*Here's the list of my Commands.*\n\n${formattedCommands}\n\n\n〽️Use *${prefix}report* to report the developer if you face any issue regarding the bot.\n\n*💗Have a nice day*\n\n*🔱 Ronen-Bots- 2023*`;
         await Atlas.sendMessage(
           m.from,
           { video: { url: botVideo }, gifPlayback: true, caption: helpText },
